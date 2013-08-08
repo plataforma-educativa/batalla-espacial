6215f36c935f23ccb516fa0addd85551d33cdb1f
marianotugnarelli@gmail.com <marianotugnarelli@gmail.com@8acefad2-8f69-11de-b0c3-d3322c7aa8e4>
2013-08-08 05:34:02 +0000

    Pruebas agregadas.

--- src/test/java/ar/com/comunidadesfera/eficiencia/test/persistencia/consultas/BuscarIdProblemaTest.java
+++ src/test/java/ar/com/comunidadesfera/eficiencia/test/persistencia/consultas/BuscarIdProblemaTest.java
@@ -3,19 +3,29 @@ package ar.com.comunidadesfera.eficiencia.test.persistencia.consultas;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
+import javax.persistence.NoResultException;
+
 import org.junit.Test;
 import org.unitils.dbunit.annotation.DataSet;
 
+@DataSet
 public class BuscarIdProblemaTest extends ConsultaTest {
 
     @Test
-    @DataSet
-    public void unProblemaEncontrado() {
+    public void encuentraUnProblemaPorNombre() {
         
         Long idProblema = (Long) this.em.createNamedQuery("buscarIdProblema")
                                         .setParameter("nombre", "Ordenamiento")
                                         .getSingleResult();
         
         assertThat("id del problema", idProblema, is(equalTo(14L)));
+    }
+    
+    @Test(expected = NoResultException.class)
+    public void noEncuentraUnProblema() {
+        
+        this.em.createNamedQuery("buscarIdProblema")
+               .setParameter("nombre", "Camino Minimo")
+               .getSingleResult();
     }
 }
